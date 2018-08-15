b5407f3c5e3beea3d3ada5a9a69999c759770581
DRBragg <DRBragg@gmail.com>
2018-08-15 16:15:36 -0400

    config head meta

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,18 @@
 module.exports = {
   title: 'Pokemon Go Updates',
   description: 'Quick Reference to Events, Bonuses, Updates, and Rumors',
+  head: [
+    ['link', { rel: 'icon', href: `/img/ball.png` }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#f57d7e' }]
+  ],
   serviceWorker: true,
   themeConfig:{
     logo: '/img/ball.png',
     lastUpdated: 'Last Updated',
+    serviceWorker: {
+      updatePopup: true
+    },
     nav: [
       { text: 'EVENTS', link: '/events/' },
       { text: 'UPDATES', link: '/updates/' },
